943bc2db0a7fa5c6c58d3c555b54aa05acb7be9a
Ganesh Pardhi <pardhiganesh07@gmail.com>
2024-08-17 14:46:36 +0530

    added changes to test file in main

---
+++ test/main.py
@@ -0,0 +1,8 @@
+## Test code for palidrom number
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Test the function
+print(is_palindrome("radar"))  # Output: True
+print(is_palindrome("hello"))  # Output: False
