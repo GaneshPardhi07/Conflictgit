ccc2998ff6939e03f2509a084421a8b01c21e224
Ganesh Pardhi <pardhiganesh07@gmail.com>
2024-08-17 14:44:54 +0530

    Added changes in main

---
+++ src/main.py
@@ -0,0 +1,15 @@
+## Main branch changes code 
+
+## prime number function inpython
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) +  1):
+        if num % i == 0:
+            return False
+    return True
+
+# Test the function
+print(is_prime(11))  # Output: True
+print(is_prime(35))  # Output: False
