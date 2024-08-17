82b0fd12a57920e015ebf2bb4e3645f45c1550c9
Ganesh Pardhi <pardhiganesh07@gmail.com>
2024-08-17 16:38:35 +0530

    added changes in rel

--- src/main.py
+++ src/main.py
@@ -14,10 +14,3 @@ def is_prime(num):
 print(is_prime(11))  # Output: True
 print(is_prime(35))  # Output: False
 
-
-def power(base, exponent):
-    return base ** exponent
-
-# Test the function
-print(power(2, 3))  # Output: 8
-print(power(3, 4))  # Output: 81
